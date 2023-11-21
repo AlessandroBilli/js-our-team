44181f5b7c5aaddd722cf8822106e3a87c23415f
AlessandroBilli <skillmanbungo@gmail.com>
2023-11-21 02:09:14 +0100

    feat: MILESTONE 1:

--- main.js
+++ main.js
@@ -1,1 +1,35 @@
-'use strict'
+const team = [
+    {
+        Nome: 'Wayne',
+        Ruolo: 'Founder & CEO',
+        Img: 'wayne-barnett-founder-ceo.jpg',
+    },
+    {
+        Nome: 'Angela',
+        Ruolo: 'Chief Editor',
+        Img: 'angela-caroll-chief-editor.jpg',
+    },
+    {
+        Nome: 'Walter',
+        Ruolo: 'Office Manager',
+        Img: 'walter-gordon-office-manager.jpg',
+    },
+    {
+        Nome: 'Angela',
+        Ruolo: 'Social Media Manager',
+        Img: 'angela-lopez-social-media-manager.jpg',
+    },
+    {
+        Nome: 'Scott',
+        Ruolo: 'Developer',
+        Img: 'scott-estrada-developer.jpg',
+    },
+    {
+        Nome: 'Barbara',
+        Ruolo: 'Graphic Designer',
+        Img: 'barbara-ramos-graphic-designer.jpg',
+    }
+];
+
+console.log(team);
+
