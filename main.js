2ea9830ae1699eff0bdd458370feff6906f6c32d
AlessandroBilli <skillmanbungo@gmail.com>
2023-11-21 02:20:59 +0100

    fix: Cognome

--- main.js
+++ main.js
@@ -1,31 +1,37 @@
 const team = [
     {
         Nome: 'Wayne',
+        Cognome: 'Barnett',
         Ruolo: 'Founder & CEO',
         Img: 'wayne-barnett-founder-ceo.jpg',
     },
     {
         Nome: 'Angela',
+        Cognome: 'Caroll',
         Ruolo: 'Chief Editor',
         Img: 'angela-caroll-chief-editor.jpg',
     },
     {
         Nome: 'Walter',
+        Cognome: 'Gordon',
         Ruolo: 'Office Manager',
         Img: 'walter-gordon-office-manager.jpg',
     },
     {
         Nome: 'Angela',
+        Cognome: 'Lopez',
         Ruolo: 'Social Media Manager',
         Img: 'angela-lopez-social-media-manager.jpg',
     },
     {
         Nome: 'Scott',
+        Cognome: 'Estrada',
         Ruolo: 'Developer',
         Img: 'scott-estrada-developer.jpg',
     },
     {
         Nome: 'Barbara',
+        Cognome: 'Ramos',
         Ruolo: 'Graphic Designer',
         Img: 'barbara-ramos-graphic-designer.jpg',
     }
